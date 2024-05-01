59cb499c941f53dbe3733029e8cd1414614c923c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-05-01 15:00:35 +0100

    setup firebase

--- aquae/lib/main.dart
+++ aquae/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MainApp());
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 class MainApp extends StatelessWidget {
