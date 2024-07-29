79c90385e996ee432a81465895de0e77a85c9389
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-29 09:59:41 +0100

    change

--- aquae/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryTextColor = Color.fromARGB(255, 133, 133, 133);
-const Color secondaryTextColor = Color.fromARGB(255, 178, 178, 178);
---
+++ lumch/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login()
+    );
+  }
+}
+
