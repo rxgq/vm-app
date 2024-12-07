e254caf7c27f7db4c58db1079d2bceba64dcc6fe
rxgq <ashtondunderdale@gmail.com>
2024-12-07 01:23:16 +0000

    added jmp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:allium/vm/machine.dart';
+import 'package:allium/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,21 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var program = [
-      0x01, 0x05, 0x01, 0x04, 0x03
-    ];
-
-    final vm = VirtualMachine(program: program);
-    final result = vm.execute();
-
-    print(result.error?.message ?? "no error");
-
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: HomeView()
     );
   }
 }
