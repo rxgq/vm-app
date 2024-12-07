ca95f08e25309547ad30232d9b9b2e7750580b39
rxgq <ashtondunderdale@gmail.com>
2024-12-07 00:04:06 +0000

    basic vm

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:allium/vm/machine.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var program = [
+      0x01, 0x05, 0x01, 0x04, 0x03
+    ];
+
+    final vm = VirtualMachine(program: program);
+    final result = vm.execute();
+
+    print(result.error?.message ?? "no error");
+
     return const MaterialApp(
       home: Scaffold(
         body: Center(
